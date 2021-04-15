59abe89f646d18af4fbea73a0cd01d02b2835085
vanillamilkk <akuneo@outlook.com>
2021-04-15 20:11:52 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,16 +5,27 @@
  *  GNU General Public License v3.0
  */
 
-const { app, BrowserWindow, Menu, ipcMain} = require('electron')
-const path = require('path')
-const exec = require('child_process').exec
-const { dialog } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain} = require('electron');
+const path = require('path');
+const exec = require('child_process').exec;
+var os = require('os');
+const { dialog } = require('electron');
+const screenshot = require('screenshot-desktop');
+const remote = require('@electron/remote/main').initialize();
 
+
+global.id = null;
+var loggedin = false;
+
+ipcMain.on( "setMyGlobalVariable", ( event, myGlobalVariableValue ) => {
+  global.id = myGlobalVariableValue;
+} );
 
 app.whenReady().then(() => {
   isRunning('csgo.exe', 'csgo_osx64', 'csgo_linux64').then((v) => {
-      if(v) {
+      if (v) {
           createWindow();
+          loop();
     } else {
         dialog.showMessageBox(null, options).then(result => {
             if (result.response === 0) {
@@ -23,6 +34,19 @@ app.whenReady().then(() => {
         }
         )}});
 })
+
+function checkLogin () {
+    if (global.id == null) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function loop () {
+     setInterval(function(){ screenshot(settings); }, 3000);
+}
+
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -43,22 +67,8 @@ function createWindow () {
     }
   })
   
-  win.removeMenu();
+ // win.removeMenu();
   win.loadFile('index.html')
-}
-
-function loadedWindow () {
-  const loaded = new BrowserWindow({
-    width: 400,
-    height: 200,
-    icon: 'icon.ico',
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-  
-  loaded.removeMenu();
-  loaded.loadFile('loaded.html')
 }
 
 function isRunning(win, mac, linux){
@@ -82,3 +92,8 @@ const options = {
     message: 'CS:GO tidak terdeteksi',
     detail: 'Tidak dapat terhubung ke proses CS:GO, harap restart game atau PC!',
   };
+  
+const settings = {
+    format: 'png',
+    filename: Date.now() + '.png'
+};
